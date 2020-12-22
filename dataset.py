f4538214497d80cd322c189bd65559b33dd06b3b
Shim Jung Hyun <shj030220@gmail.com>
2020-12-22 17:17:53 +0900

    [Feat] 학습 데이터셋 loading

--- dataset.py
+++ dataset.py
@@ -1,1 +1,5 @@
 import pandas as pd
+
+train_data = pd.read_csv("./dataset/train.csv")
+train_data["DataTime"] = pd.to_datetime(train_data.DataTime)
+train_data["date"] = train_data.DateTime.dt.date
