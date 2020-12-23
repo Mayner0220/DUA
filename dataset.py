9a048480a108d20225c26603a307c2a20d3722b1
Shim Jung Hyun <shj030220@gmail.com>
2020-12-23 16:12:06 +0900

    [Fea] 데이터셋 전처리

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-train_data = pd.read_csv("./dataset/train.csv")
-train_data["DataTime"] = pd.to_datetime(train_data.DataTime)
+train_data = pd.read_csv("./dataset/train.csv", encoding="euc-kr")
+train_data["DataTime"] = pd.to_datetime(train_data["DataTime"])
 train_data["date"] = train_data.DateTime.dt.date
+train_data = train_data("data").sum().reset_index()
