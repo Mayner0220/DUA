f6b3ca5d05c15588b3007630329eb50f1446ad3a
Shim Jung Hyun <shj030220@gmail.com>
2020-12-25 00:34:41 +0900

    [Feat] 데이터셋 전처리

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 train_data = pd.read_csv("./dataset/train.csv", encoding="euc-kr")
 train_data["DateTime"] = pd.to_datetime(train_data.DateTime)
@@ -9,3 +10,8 @@ mini = train_data.iloc[:, 1:].min()
 size = train_data.iloc[:,1:].max() - train_data.iloc[:,1:].min()
 train_data.iloc[:,1:] = (train_data.iloc[:,1:] -  mini) / size
 
+input_window = 30
+output_window = 7
+
+window_x = np.zeros((train.shape[0] - (input_window + output_window), input_window, 4))
+window_y = np.zeros((train.shape[0] - (input_window + output_window), output_window, 4))
