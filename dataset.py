3342a1d335cc34e35a8fd236cd89de768a29efca
Shim Jung Hyun <shj030220@gmail.com>
2020-12-24 21:50:49 +0900

    [Feat] 데이터셋 전처리

--- dataset.py
+++ dataset.py
@@ -3,4 +3,9 @@ import pandas as pd
 train_data = pd.read_csv("./dataset/train.csv", encoding="euc-kr")
 train_data["DateTime"] = pd.to_datetime(train_data.DateTime)
 train_data["date"] = train_data.DateTime.dt.date
-train_data = train_data("data").sum().reset_index()
+train  = train_data.groupby("date").sum().reset_index()
+
+mini = train_data.iloc[:, 1:].min()
+size = train_data.iloc[:,1:].max() - train_data.iloc[:,1:].min()
+train_data.iloc[:,1:] = (train_data.iloc[:,1:] -  mini) / size
+
