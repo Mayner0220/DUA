2d53045689812cae96f32d943bda33bdde068992
Shim Jung Hyun <shj030220@gmail.com>
2021-01-15 22:14:44 +0900

    [Feat] Model init

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
+from tensorflow.keras.layers import *
 
 class Model(tf.keras.Model):
     def __init__(self):
         super(Model, self).__init__()
-        self.
+        self.lstm = LSTM()
